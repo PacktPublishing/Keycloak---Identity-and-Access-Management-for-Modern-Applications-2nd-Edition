bf51a245a3e336c3464e152c749a58776d8075a7
philipz <philipzheng@gmail.com>
2023-08-11 17:42:53 +0800

    Fixed Ch7 Spring Boot package.

--- ch7/springboot/backend/src/main/java/org/keycloak/examples/HelloController.java
+++ ch7/springboot/backend/src/main/java/org/keycloak/examples/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.springboot;
+package org.keycloak.examples;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
